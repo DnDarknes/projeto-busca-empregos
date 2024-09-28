3efb735939aba00c89d35edd960fc07d5660db6c
Bruno Dutra Coelho <79719357+DnDarknes@users.noreply.github.com>
2024-09-28 12:42:32 -0300

    Update express.d.ts

--- express.d.ts
+++ express.d.ts
@@ -3,7 +3,7 @@ import { Request } from "express";
 declare global {
     namespace Express {
         interface Request {
-            userId?: string; // ou mongoose.Types.ObjectId dependendo do que você está usando
+            userId?: string;
         }
     }
 }
